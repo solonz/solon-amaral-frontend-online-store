85789ac1d296b2777c07ecdd8606fc670ceaf912
Amadeus Dias <amadeus@pop-os.localdomain>
2022-07-07 16:55:58 -0300

    requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,22 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const requisicao = await fetch(ENDPOINT);
+    const data = await requisicao.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+    const ENDPOINT1 = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const requisicao = await fetch(ENDPOINT1);
+    const data = await requisicao.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
