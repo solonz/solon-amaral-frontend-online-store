ecc64ec8ee9a50bf1284781a5d7e61cf5c0288fc
Amadeus Dias <amadeus@pop-os.localdomain>
2022-07-07 17:16:36 -0300

    correção da correção

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ export async function getCategories() {
   try {
     const ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/categories';
     const request = await fetch(ENDPOINT);
-    const data = await requisicao.json();
+    const data = await request.json();
     return data;
   } catch (error) {
     console.error(error);
@@ -14,7 +14,7 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
     const ENDPOINT1 = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
     const request = await fetch(ENDPOINT1);
-    const data = await requisicao.json();
+    const data = await request.json();
     return data;
   } catch (error) {
     console.error(error);
