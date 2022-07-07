03301c9648cddbff5fcd6922c3b211a2e7280d58
amadeus <82623135+amadeusdias@users.noreply.github.com>
2022-07-07 17:10:34 -0300

    Update src/services/api.js
    Co-authored-by: Carlos Henryck <58481753+carloshenryck@users.noreply.github.com>

--- src/services/api.js
+++ src/services/api.js
@@ -13,7 +13,7 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   try {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
     const ENDPOINT1 = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-    const requisicao = await fetch(ENDPOINT1);
+    const request = await fetch(ENDPOINT1);
     const data = await requisicao.json();
     return data;
   } catch (error) {
