50a7193d87a2c3f3fcbbfbcd584504be481b5002
Solon <amaral.solon@hotmail.com>
2022-07-11 16:45:39 -0300

    Problema 8

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class ProductList extends React.Component {
+  addToCart = (event) => {
+    let cart = JSON.parse(localStorage.getItem('productId'));
+    console.log(cart);
+    if (!cart) {
+      cart = [];
+    }
+    cart.push(event.target.id);
+    localStorage.setItem(('productId', JSON.stringify(cart)));
+  }
+
   render() {
     const {
       productList,
@@ -13,17 +23,31 @@ class ProductList extends React.Component {
           ? <p>Nenhum produto foi encontrado</p>
           : (
             productList.map((product) => (
-              <Link
-                data-testid="product-detail-link"
-                to={ `/productDetails/${product.id}` }
-                key={ product.id }
-              >
-                <div data-testid="product">
-                  <h3>{ product.title }</h3>
-                  <img src={ product.thumbnail } alt={ product.title } />
-                  <p>{ product.price }</p>
-                </div>
-              </Link>
+              <>
+                <Link
+                  data-testid="product-detail-link"
+                  to={ `/productDetails/${product.id}` }
+                  key={ product.id }
+                >
+                  <div data-testid="product">
+                    <h3>{ product.title }</h3>
+                    <img src={ product.thumbnail } alt={ product.title } />
+                    <p>{ product.price }</p>
+                  </div>
+                  <div />
+                </Link>
+                <button
+                  data-testid="product-add-to-cart"
+                  id={ product.id }
+                  type="button"
+                  onClick={ this.addToCart }
+                >
+                  {' '}
+                  Comprar
+                  {' '}
+
+                </button>
+              </>
             ))
           )}
       </div>
